a29dc13e148a3204722b30bb0eba9dd6e6eabb33
SuperWarioGalaxy <thecodyrex6333@gmail.com>
2024-11-11 18:24:32 +0000

    creacion de users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from users.models import Profile
+
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'avatar',
+        'bio',
+        'user',
+    ]
